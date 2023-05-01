9517a28abe7016bd26c432beff8390a73b9d9cb1
Zac Hogan <zacvanhogan@gmail.com>
2023-05-01 15:23:51 +1200

    feat: implement basic conversation with ChatGPT 3.5 Turbo

--- php/frontController.php
+++ php/frontController.php
@@ -4,17 +4,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-// send request to backend
-// expect to recieve back 1x image, and 1x chat message
+switch ($_POST['queryType']) {
+    case 'generate':
+        include "../../../private/virtualinnkeeper/php/generateNPC.php";
+        break;
 
-// Check for request type ($_POST['queryType'])
-// then...
-// Temporary - just handle requests as though GENERATE requests
+    case 'interact':
+        include "../../../private/virtualinnkeeper/php/interactNPC.php";
+        break;
+    
+    default:
+        echo "Invalid queryType";
+}
 
-// TODO: convert this into a POST request using CURL or similar - this will avoid any
-// conflicting variable/function names, etc.
-//include "../../../private/virtualinnkeeper/php/generateNPC.php";
 
-include "../../../private/virtualinnkeeper/php/generateNPC.php";
+
+
 
 ?>
--- scripts/buttonInteractNPC.js
+++ scripts/buttonInteractNPC.js
@@ -1,13 +1,12 @@
-(function(){
+(function (){
 
-    const generateForm = document.querySelector("#generateForm");
-    const generateButton = document.querySelector("#generateButton");
+    const interactForm = document.querySelector("#interactForm");
+    const interactButton = document.querySelector("#interactButton");
 
 
-
-    generateButton.addEventListener("click", event => {
-        event.preventDefault();            
-
+    interactButton.addEventListener("click", event => {
+        event.preventDefault();   
+        
         // Generate formData object for use in later POST request
         let formData = buildQuery();
 
@@ -21,34 +20,31 @@
     });
 
 
-    function buildQuery(){
-        let form = generateForm;
+    function buildQuery(){    
+        let form = document.querySelector("#interactForm");
         let formData = new FormData(form);        
-        formData.append("queryType", "generate");        
+        formData.append("queryType", "interact");
         return formData;
     }
 
-    async function sendQuery(formData){
-        console.log("send");
 
+    async function sendQuery(formData){
         // Fetch data from backend via FEController
             // Note: Fetch + POST uses a FormData object rather than a GET query string URL
         let response = await fetch("./php/frontController.php", {method: 'POST', body: formData});
-        let data = await response.text();           
+        let data = await response.text();  
         
-
+        // Append returned data response to DOM #response object
         let responseBox = document.querySelector("#response");       
         let responseElement = document.createElement("p");        
         responseElement.innerHTML = data;
         responseBox.appendChild(responseElement);  
-
-        
     }
+        
 
 
     function processResponse(response){
 
         
     }
-
 }());
